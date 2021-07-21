de6ec053670a7bccf03d3f7bf44c7f219dc3db78
Emily Foley <efoley326@gmail.com>
2021-07-21 10:38:00 -0400

    file set up

---
+++ questions/models.py
@@ -0,0 +1,9 @@
+import datetime
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+
+class User(AbstractUser):
+    def __str__(self):
+        return self.username
