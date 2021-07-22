349372dc963cae3c226fcb0470b9ca0fe0fc6281
itsdeebarnett <jdeebarnett@gmail.com>
2021-07-22 11:18:12 -0400

    models and views

--- questions/views.py
+++ questions/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Questions, Answer, Tag
+# from .serializers import 
 
 # Create your views here.
+class QuestionList(generics.ListCreateAPIView):
+    queryset = Questions.objects.all()
+
+
+class QuestionDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Questions.objects.all()
+
+
+
+class AnswerList(generics.ListCreateAPIView):
+    Questions = Answer.objects.all()
+
+
+
+class AnswerDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Answer.objects.all()
+    
