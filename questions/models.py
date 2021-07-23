b3ec4495f51f6322b3f0046d4a23e924537fd91a
Emily Foley <efoley326@gmail.com>
2021-07-23 16:05:57 -0400

    serializers and views

---
+++ questions/models.py
@@ -0,0 +1,35 @@
+import datetime
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models.deletion import CASCADE
+
+
+
+
+class User(AbstractUser):
+    def __str__(self):
+        return self.username
+
+
+class Question(models.Model):
+    question = models.TextField()
+    user = models.ForeignKey(User, on_delete=CASCADE, related_name='question_user')
+    created_at = models.DateField(auto_now_add=True)
+    favorited_by = models.ManyToManyField(User, related_name='favorited', null=True, blank=True)
+    tag = models.ManyToManyField('Tag', related_name='questions', null=True, blank=True)
+
+
+class Answer(models.Model):
+    answer = models.TextField()
+    user = models.ForeignKey(User, on_delete=CASCADE, related_name='answer_user')
+    created_at = models.DateField(auto_now_add=True)
+    accepted = models.BooleanField()
+    favorited = models.BooleanField()
+    question = models.ForeignKey(Question, on_delete=CASCADE)
+    
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.tag
--- questions/views.py
+++ questions/views.py
@@ -1,32 +1,90 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .models import Questions, Answer, Tag
+from .models import Question, Answer, Tag, User
+from library.serializers import QuestionSerializer, AnswerSerializer, UserSerializer, TagSerializer
+from rest_framework import generics, status
+from rest_framework.permissions import IsAdminUser
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.parsers import JSONParser
+import io
 # from .serializers import 
 
 # Create your views here.
+
 class QuestionList(generics.ListCreateAPIView):
-    queryset = Questions.objects.all()
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+    permission_classes = [User]
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = Question.Serializer(queryset, many=True)
+        return Response(serializer.data)
 
 
 class QuestionDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Questions.objects.all()
+    queryset = Question.objects.all()
 
+    def get_question(self, pk):
+        return Question.objects.get(pk=pk)
 
+    def put(self, request, pk, format=None):
+        question = self.get_object(pk)
+        serializer = QuestionSerializer(question, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
 
+    def delete(self, request, pk, format=None):
+        question = self.get_object(pk)
+        question.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    
 class AnswerList(generics.ListCreateAPIView):
     queryset = Answer.objects.all()
+    serializer_class =  AnswerSerializer
 
-
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = AnswerSerializer(queryset, many=True)
+        return Response(serializer.data)
 
 class AnswerDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Answer.objects.all()
+
+    def get_answer(self, pk):
+        return Answer.objects.get(pk=pk)
+
+    def put(self, request, pk, format=None):
+        book = self.get_object(pk)
+        serializer = AnswerSerializer(book, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+            
 
 
 
 class TagList(generics.ListCreateAPIView):
     queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = TagSerializer(queryset, many=True)
+        return Response(serializer.data)
 
 
 
 class TagDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+    def put(self, request, pk, format=None):
+        tag = self.get_object(pk)
+        serializer = TagSerializer(tag, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
