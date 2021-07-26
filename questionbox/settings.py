afe84591b567e6e002cee8412732f9b238e41968
Emily Foley <efoley326@gmail.com>
2021-07-26 09:50:42 -0400

    cors edit

--- questionbox/settings.py
+++ questionbox/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import environ
 import os
+from corsheaders.defaults import default_headers
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,7 +84,9 @@ TEMPLATES = [
 
 AUTH_USER_MODEL = "questions.User"
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_HEADERS = list(default_headers) + [
+    'content-disposition',
+]
 
 CORS_ALLOW_METHODS = [
     'DELETE',
