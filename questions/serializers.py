ddcad86f1c1e08a6d7999eceedcb5937b53ba21e
Emily Foley <efoley326@gmail.com>
2021-07-27 19:25:14 -0400

    userserializer edit and view

--- questions/serializers.py
+++ questions/serializers.py
@@ -1,5 +1,13 @@
 from rest_framework import serializers
 from .models import User, Question, Answer, Tag
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
