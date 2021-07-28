80b69fca235379a161ca0b7c080bb6154d17f8bd
Emily Foley <efoley326@gmail.com>
2021-07-28 15:31:47 -0400

    serializer reorder

--- questions/serializers.py
+++ questions/serializers.py
@@ -2,10 +2,15 @@ from rest_framework import serializers
 from .models import User, Question, Answer, Tag
 
 class UserSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = ['question', 'user', 'created_at', 'favorited_by', 'tag']
+        read_only_fields = ['user', 'created_at']
 
 class AnswerSerializer(serializers.ModelSerializer):
     class Meta:
@@ -18,11 +23,3 @@ class TagSerializer(serializers.ModelSerializer):
         model = Tag
         fields = ['tag']
         read_only_fields = ['tag']
-
-class QuestionSerializer(serializers.ModelSerializer):
-    answers = AnswerSerializer(many=True, read_only=True)
-    tags = TagSerializer(many=True, read_only=True)
-    class Meta:
-        model = Question
-        fields = ['question', 'user', 'created_at', 'favorited_by', 'tag']
-        read_only_fields = ['user', 'created_at']
