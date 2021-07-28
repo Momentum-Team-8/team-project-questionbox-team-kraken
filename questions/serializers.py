918a636a02e5d44439808ca3c97d1f4cfd16c7cf
Emily Foley <efoley326@gmail.com>
2021-07-28 13:48:26 -0400

    serializer order

--- questions/serializers.py
+++ questions/serializers.py
@@ -7,24 +7,22 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['username', 'email', 'password']
 
-
-class QuestionSerializer(serializers.ModelSerializer):
-    answers = "AnswerSerializer"(many=True, read_only=True)
-    tags = "TagSerializer"(many=True, read_only=True)
-    class Meta:
-        model = Question
-        fields = ['question', 'user', 'created_at', 'favorited_by', 'tag']
-        read_only_fields = ['user', 'created_at']
-
 class AnswerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Answer
         fields = ['answer', 'user', 'created_at', 'accepted', 'favorited', 'question']
         read_only_fields = ['user', 'created_at', 'accepted', 'question']
 
-
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = ['tag']
         read_only_fields = ['tag']
+
+class QuestionSerializer(serializers.ModelSerializer):
+    answers = AnswerSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
+    class Meta:
+        model = Question
+        fields = ['question', 'user', 'created_at', 'favorited_by', 'tag']
+        read_only_fields = ['user', 'created_at']
